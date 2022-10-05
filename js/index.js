07f8b23207c245f06a51ec4bd359437fee55702e
Fengs <43527690+Fengs68@users.noreply.github.com>
2022-10-05 22:15:58 +0800

    Add files via upload
    aa

---
+++ js/index.js
@@ -0,0 +1,35 @@
+$(this).scroll(() => {
+	let a = $(this).scrollTop()
+	if (a === 0) {
+		$(".navbar")
+			.removeClass("bg-dark")
+			.css("padding-top", "16px")
+			.find(".nav-link")
+			.removeClass("text-white")
+	} else {
+		$(".navbar")
+			.addClass("bg-dark")
+			.css("padding-top", "0px")
+			.find(".nav-link")
+			.addClass("text-white")
+	}
+})
+$(".source-item")
+	.mouseenter(function() {
+		$(this)
+			.parent()
+			.animate({
+				top: "-10px"
+			}, 250)
+	}).mouseleave(function() {
+		$(this)
+			.parent()
+			.animate({
+				top: "0px"
+			}, 250)
+			
+	})
+
+$(".carousel").carousel({
+	interval: 2000
+})
